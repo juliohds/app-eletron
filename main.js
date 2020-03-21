740bdc8fdcbb94c0c61b35c9b69bb008a3fca783
Gabriel Vacari <gabriel_vacari@outlook.com>
2020-03-21 19:16:40 -0300

    generating dmg for macos

--- main.js
+++ main.js
@@ -7,8 +7,12 @@ function createWindow () {
     height: 600,
     webPreferences: {
       nodeIntegration: true
-    }
+    },
+    icon: __dirname + '/assets/icons/png/zoe_icon.png'
   })
+
+  win.setIcon(__dirname + '/assets/icons/png/zoe_icon.png')
+
 
   // e carregar o index.html do aplicativo.
   win.loadFile('index.html')
---
+++ readme.md
@@ -0,0 +1,8 @@
+
+
+-- sistema que vc cadastra seu perfil.
+
+# 1 - personalizar o icone do dock do MacOS
+# 2 - tirar uma foto
+# 3 - notificacao (completar o perfil)
+# 4 - gerar instalador do electron do MacOS is DONE!!!!!!!
