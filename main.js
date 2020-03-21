15a1efc759956159af145109db5525ba73da5b32
Gabriel Vacari <gabriel_vacari@outlook.com>
2020-03-21 19:34:20 -0300

    fix for text in app

--- main.js
+++ main.js
@@ -3,17 +3,12 @@ const { app, BrowserWindow } = require('electron')
 function createWindow () {
   // Cria uma janela de navegação.
   let win = new BrowserWindow({
-    width: 800,
-    height: 600,
+    width: 1200,
+    height: 1200,
     webPreferences: {
       nodeIntegration: true
     },
-    icon: __dirname + '/assets/icons/png/zoe_icon.png'
   })
-
-  win.setIcon(__dirname + '/assets/icons/png/zoe_icon.png')
-
-
   // e carregar o index.html do aplicativo.
   win.loadFile('index.html')
 }
--- readme.md
+++ readme.md
@@ -2,7 +2,9 @@
 
 -- sistema que vc cadastra seu perfil.
 
-# 1 - personalizar o icone do dock do MacOS
-# 2 - tirar uma foto
-# 3 - notificacao (completar o perfil)
-# 4 - gerar instalador do electron do MacOS is DONE!!!!!!!
+# 1 - personalizar o icone do dock do MacOS -- DONE
+# 2 - Personalizar o icone da barra de tarefas no windows
+# 3 - tirar uma foto
+# 4 - notificacao (completar o perfil)
+# 5 - gerar instalador do electron do MacOS -- DONE
+# 6 - Gerar o instalador do app para o Windows
