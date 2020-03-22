d2730a6d0265e6a683962a26737ef9f86c0b0431
unknown <marcosmss1304@gmail.com>
2020-03-22 10:39:34 -0300

    Fix bug in prod and add trail zoeIcon

--- main.js
+++ main.js
@@ -1,31 +1,28 @@
-const { app, BrowserWindow, Tray, Menu } = require('electron');
-const path = require('path');
-function createWindow () {
+const { app, BrowserWindow, Tray, Menu } = require("electron");
+const path = require("path");
+function createWindow() {
   // Cria uma janela de navegação.
   let win = new BrowserWindow({
     width: 1200,
     height: 1200,
     webPreferences: {
       nodeIntegration: true
-    },
-  })
-  const iconPath = path.join(__dirname, 'assets/iconTemplate.png');
-  
-  tray = new Tray(iconPath)
+    }
+  });
+  const iconPath = path.join(__dirname, "assets/zoe_icon.png");
+
+  tray = new Tray(iconPath);
   const contextMenu = Menu.buildFromTemplate([
-    { label: 'Item1', type: 'radio' },
-    { label: 'Item2', type: 'radio' },
-    { label: 'Item3', type: 'radio', checked: true },
-    { label: 'Item4', type: 'radio' }
-  ])
-  tray.setToolTip('This is my application.')
-  tray.setContextMenu(contextMenu)
+    { label: "Item1", type: "radio" },
+    { label: "Item2", type: "radio" },
+    { label: "Item3", type: "radio", checked: true },
+    { label: "Item4", type: "radio" }
+  ]);
+  tray.setToolTip("This is my application.");
+  tray.setContextMenu(contextMenu);
 
   // e carregar o index.html do aplicativo.
-  win.loadFile('index.html')
+  win.loadFile("index.html");
 }
 
-app.whenReady().then(createWindow)
-
-
-
+app.whenReady().then(createWindow);
