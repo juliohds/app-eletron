557043918ab140568798f14f737822f60816c4b3
Julio Henrique <juliohenrique@Julios-MacBook-Air.local>
2020-03-21 21:51:50 -0300

    test new path

--- main.js
+++ main.js
@@ -10,7 +10,7 @@ function createWindow () {
     },
   })
 
-  tray = new Tray('iconTemplate.png')
+  tray = new Tray('assets/iconTemplate.png')
   const contextMenu = Menu.buildFromTemplate([
     { label: 'Item1', type: 'radio' },
     { label: 'Item2', type: 'radio' },
